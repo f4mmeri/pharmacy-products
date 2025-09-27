a286630f88593ad16661e74a7c893202694a92db
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-09-27 11:38:22 -0500

    refactor: improve overall project structure

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import Base, engine, wait_for_db
 from app.productos.controller import router as productos_router
 from app.core.seed import seed_db
@@ -27,8 +28,22 @@ app = FastAPI(
     redoc_url="/redoc"
 )
 
+# Configurar CORS
+origins = [
+    "http://localhost:5173",    # Vite
+    "http://127.0.0.1:5173",    # Vite con 127.0.0.1
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 # Incluir los routers
-app.include_router(productos_router)
+app.include_router(productos_router, prefix="/api")  # Agregué el prefijo /api
 
 # Poblar la base de datos con datos iniciales
 logger.info("Ejecutando seed de la base de datos...")
