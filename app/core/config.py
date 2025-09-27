d0b58b6419d65c45c304ab68e0f100c7b8d4474c
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-09-27 01:40:38 -0500

    refactor: improve overall project structure

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Settings:
-    DATABASE_URL = "sqlite:///./farmacia.db"
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL",
+        "mysql+mysqlconnector://user:password@localhost:3306/farmacia_db"
+    )
 
 settings = Settings()
