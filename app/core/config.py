7cca8de6026f463a11ac081b53d37ec3186164b7
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-09-27 01:47:40 -0500

    refactor: improve overall project structure

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ import os
 class Settings:
     DATABASE_URL = os.getenv(
         "DATABASE_URL",
-        "mysql+mysqlconnector://user:password@localhost:3306/farmacia_db"
+        "mysql+pymysql://farmacia_user:farmacia_pass@localhost:3306/farmacia_db"
     )
 
 settings = Settings()
